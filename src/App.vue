d192c6f1b7c36b5107289b57404d97e0f5511a44
Lucas Lopes <lucaslopesf2@gmail.com>
2018-04-19 18:58:57 -0300

    Style updates
    Now each component has its specific style

--- src/App.vue
+++ src/App.vue
@@ -15,35 +15,12 @@ export default {
 }
 </script>
 
-<style>
-* {
-  background-color: rgb(51, 51, 51);
-  margin: 0;
-  padding: 0;
-  overflow-y: hidden;
-  overflow-x: hidden;
-}
-
-.inline {
-  position: absolute;
-  margin: auto;
-  left: 0;
-  right: 0;
-  max-width: 75%;
-}
-
-.inline li {
-  list-style: none;
-  display: flex;
-  margin: 15pt;
-  display: inline-block;
-  justify-content: center;
-}
-
-.inline svg {
+<style scoped>
+.change {
+  position: fixed;
   width: 50pt;
   height: 50pt;
-  fill: rgb(255, 255, 255);
-  cursor: pointer;
+  right: 10px;
+  bottom: 10px
 }
 </style>
--- src/components/networks/Networks.vue
+++ src/components/networks/Networks.vue
@@ -23,3 +23,41 @@ export default {
     }
   }
 }
+</script>
+
+<style>
+* {
+  background-color: rgb(51, 51, 51);
+  margin: 0;
+  padding: 0;
+  overflow-y: hidden;
+  overflow-x: hidden;
+}
+
+.inline {
+  position: absolute;
+  margin: auto;
+  left: 0;
+  right: 0;
+  max-width: 75%;
+}
+
+.inline li {
+  list-style: none;
+  display: flex;
+  margin: 15pt;
+  display: inline-block;
+  justify-content: center;
+}
+
+.inline svg {
+  width: 50pt;
+  height: 50pt;
+  fill: rgb(255, 255, 255);
+  cursor: pointer;
+}
+
+.change {
+  background-color: rgb(255, 255, 255);
+}
+</style>
