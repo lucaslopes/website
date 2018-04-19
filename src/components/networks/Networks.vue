f741c4d984b4f4adf058de23af47fe144744de92
Lucas Lopes <lucaslopesf2@gmail.com>
2018-04-19 18:54:21 -0300

    Create component Social
    Component Social has all the properties of a social network link, such as 'title', 'url' and the image path

---
+++ src/components/networks/Networks.vue
@@ -0,0 +1,13 @@
+<template>
+<ul class="inline">
+  <li v-for="network in networks">
+    <social :title="network.title" :url="network.url" :viewBox="network.viewBox" :path="network.path"></social>
+  </li>
+</ul>
+</template>
+
+<script>
+import Social from '../shared/social/Social.vue'
+  components: {
+    social: Social
+  },
---
+++ src/components/shared/social/Social.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <a :title="title" :href="url" target="_blank" rel="noopener noreferrer">
+    <span>
+      <svg xmlns="http://www.w3.org/2000/svg" :viewBox="viewBox">
+        <path :d="path" fill-rule="evenodd"/>
+      </svg>
+    </span>
+  </a>
+</template>
+
+<script>
+export default {
+  props: ['title', 'url', 'viewBox', 'path']
+}
+</script>
+
+<style scoped>
+</style>
