d677550500f5b97e50a91d09d6ba1b773b730ab8
Lucas Lopes <lucaslopesf2@gmail.com>
2018-04-19 23:35:26 -0300

    Display the projects
    The Project component display all projects, where each of them is a Panel component.

--- src/components/projects/Projects.vue
+++ src/components/projects/Projects.vue
@@ -1,5 +1,10 @@
 <template lang="html">
   <h1>Foi!</h1>
+  <ul>
+    <li v-for="project in projects">
+      <panel class="painel" :image="project.image" :name="project.name" :demo="project.demo" :info="project.info" :code="project.code"></panel>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -7,6 +12,9 @@ import ProjectPanel from '../shared/project-panel/ProjectPanel.vue'
 import { projects } from '../../assets/projects.js'
 
 export default {
+  components: {
+    panel: ProjectPanel
+  },
 
   data() {
     return {
