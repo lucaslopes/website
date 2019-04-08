c11025bd8bd44f7053b6781ef2292af715e1583f
Lucas Lopes <lucaslopesf2@gmail.com>
2019-04-08 18:12:21 -0300

    Mask email addess

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 let social = [
-{color: '#AD35AF', img: 'imgs/instagram.png', url: 'https://instagram.com/lucaslopesf'},
-{color: '#3C5999', img: 'imgs/facebook.png',  url: 'https://fb.com/lucaslopes42'},
-{color: '#0274B3', img: 'imgs/linkedin.png',  url: 'https://linkedin.com/in/lucaslopesf'},
-{color: '#189CDE', img: 'imgs/telegram.png',  url: 'https://t.me/lucaslopesf'},
-{color: '#2AA9E0', img: 'imgs/twitter.png',   url: 'https://twitter.com/lucaslopes37'},
-{color: '#DC2A26', img: 'imgs/youtube.png',   url: 'https://youtube.com/lucaslopesf2'},
-{color: '#1DB854', img: 'imgs/spotify.png',   url: 'https://open.spotify.com/user/12146452710'},
-{color: '#1B1F23', img: 'imgs/github.png',    url: 'https://github.com/lucaslopes'},
-{color: '#292929', img: 'imgs/medium.png',    url: 'https://medium.com/@lucaslopesf'},
-{color: '#2C2B64', img: 'imgs/lattes.png',    url: 'http://lattes.cnpq.br/4977148353055436'},
-{color: '#B92A27', img: 'imgs/quora.png',     url: 'https://quora.com/profile/Lucas-Lopes-3'},
-{color: '#F6F6F6', img: 'imgs/email.png',     url: 'email', email: 'lucaslopesf2@gmail.com'}
+	{color: '#AD35AF', img: 'imgs/instagram.png', url: 'https://instagram.com/lucaslopesf'},
+	{color: '#3C5999', img: 'imgs/facebook.png',  url: 'https://fb.com/lucaslopes42'},
+	{color: '#0274B3', img: 'imgs/linkedin.png',  url: 'https://linkedin.com/in/lucaslopesf'},
+	{color: '#189CDE', img: 'imgs/telegram.png',  url: 'https://t.me/lucaslopesf'},
+	{color: '#2AA9E0', img: 'imgs/twitter.png',   url: 'https://twitter.com/lucaslopes37'},
+	{color: '#DC2A26', img: 'imgs/youtube.png',   url: 'https://youtube.com/lucaslopesf2'},
+	{color: '#1DB854', img: 'imgs/spotify.png',   url: 'https://open.spotify.com/user/12146452710'},
+	{color: '#1B1F23', img: 'imgs/github.png',    url: 'https://github.com/lucaslopes'},
+	{color: '#292929', img: 'imgs/medium.png',    url: 'https://medium.com/@lucaslopesf'},
+	{color: '#2C2B64', img: 'imgs/lattes.png',    url: 'http://lattes.cnpq.br/4977148353055436'},
+	{color: '#B92A27', img: 'imgs/quora.png',     url: 'https://quora.com/profile/Lucas-Lopes-3'},
+	{color: '#F6F6F6', img: 'imgs/email.png',     url: 'email', email: 'luc' + 'aslop' + 'esf2@g' + 'mai' + 'l.c' + 'om'}
 ];
 
 let table;
@@ -68,7 +68,7 @@ $(window).on('shake', function() {
 });
 
 let message = $('<div>').addClass('message');
-message.html('E-mail address copied to clipboard');
+message.html('Email address copied to clipboard');
 
 $(function() {
 	$('body').append(table);
@@ -112,9 +112,8 @@ function copy(string) {
 		el.setSelectionRange(0, 999999);
 		el.contentEditable = editable;
 		el.readOnly = readOnly;
-	} else {
+	} else
 		textarea.select();
-	}
 	document.execCommand('copy');
 	textarea.remove();
 }
